070e612b954b737c6455c02fe2fae3082db29067
Lokendra Nath <nathdeveloper25@gmail.com>
2025-12-09 10:52:36 +0530

    ddl

---
+++ 29 Master SQL/230_DDL.sql
@@ -0,0 +1,19 @@
+-- Alter
+  -- Add Colounm
+    ALTER TABLE shoe_store ADD COLUMN hand NUMERIC;
+
+  -- Drop Colounm
+    ALTER TABLE shoe_store DROP COLUMN hand;
+
+-- DROP TABLE
+  DROP TABLE EXAMPLETABLE
+
+-- Truncate
+  TRUNCATE TABLE examplTable
+
+-- Rename
+  ALTER TABLE examplTable RENAME exampleTable2
+
+
+
+SELECT * FROM shoe_store;
